a9a2f171b121422787aa2f8836e49cab88a6e987
Jesús Enrique Rascón <blograso@gmail.com>
2025-05-19 14:35:06 +0200

    init commit

---
+++ src/lib/components/ui/Sidebar.svelte
@@ -0,0 +1,11 @@
+<script>
+	const fakeConvs = Array.from({ length: 20 }, (_, i) => i + 1);
+
+	console.log(fakeConvs);
+</script>
+
+{#each fakeConvs as conv}
+	<div class="flex items-center gap-2">
+		<div>{conv}</div>
+	</div>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<main class="mx-auto my-20 max-w-3xl text-xl">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</main>
